67812b8a3f9b53ba6897a0795093dd4cafa864e5
dheerajkumard <dheerajkumarddk@gmail.com>
2025-07-21 19:27:09 +0530

    playing with routing

--- src/app.js
+++ src/app.js
@@ -1,6 +1,29 @@
 const express = require("express");
 
 const app = express();
+// dynamic routing
+app.get("/user/:userId/:name/:password", (req, res) => {
+    console.log(req.params);
+    res.send({firstname: "Dheeraj", lastname: "Kumar"});
+});
+
+// query param
+app.get("/user1", (req, res) => {
+    console.log(req.query);
+    res.send({firstname: "Dheeraj", lastname: "Kumar"});
+});
+
+app.get("/user", (req, res) => {
+    res.send("In User page");
+});
+
+app.post("/user", (req, res) => {
+    res.send("posting the data");
+});
+
+app.delete("/user", (req, res) => {
+    res.send("deleting the data");
+});
 
 app.use("/home", (req,res) => {
     res.send("In Home page");

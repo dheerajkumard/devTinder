127251a8c7bfaeb1de9b477373629bf5f3245931
dheerajkumard <dheerajkumarddk@gmail.com>
2025-07-22 08:23:49 +0530

    playing with multiple routing handlers

--- src/app.js
+++ src/app.js
@@ -1,44 +1,14 @@
 const express = require("express");
 
 const app = express();
-// dynamic routing
-app.get("/user/:userId/:name/:password", (req, res) => {
-    console.log(req.params);
-    res.send({firstname: "Dheeraj", lastname: "Kumar"});
-});
 
-// query param
-app.get("/user1", (req, res) => {
-    console.log(req.query);
-    res.send({firstname: "Dheeraj", lastname: "Kumar"});
-});
-
-app.get("/user", (req, res) => {
-    res.send("In User page");
-});
-
-app.post("/user", (req, res) => {
-    res.send("posting the data");
-});
-
-app.delete("/user", (req, res) => {
-    res.send("deleting the data");
-});
-
-app.use("/home", (req,res) => {
-    res.send("In Home page");
-});
-
-app.use("/dashboard", (req,res) => {
-    res.send("In dashboard page");
-});
-
-app.use("/test", (req,res) => {
-    res.send("testing the server!!");
-});
-
-app.use((req,res) => {
-    res.send("Hello World!");
+//multiple route handlers
+app.get("/user", (req, res, next) => {
+    console.log("First handler");
+    next();
+}, (req, res) => {
+    console.log("Second handler");
+    res.send("Multiple route handlers executed");  //output will be "Multiple route handlers executed"
 });
 
 app.listen(3000, () => {

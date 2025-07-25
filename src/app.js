a16b084048fc04fe9fa30d23f275d81927261496
dheerajkumard <dheerajkumarddk@gmail.com>
2025-07-25 12:35:47 +0530

    get, delete, patch apis

--- src/app.js
+++ src/app.js
@@ -26,6 +26,55 @@ app.post("/signup", async (req, res) => {
     }
 
 })
+// Fetching user data based on email
+app.get("/user", async (req, res) => {
+    try {
+        const userEmail = req.body.email;
+        const users = await User.find({ email: userEmail });
+        res.send(users);
+    }
+    catch (err) {
+        res.status(500).send("Error fetching users");
+    }
+})
+// Fetching all users
+app.get("/feed", async (req, res) => {
+    try {
+
+        const users = await User.find({});
+        res.send(users);
+    }
+    catch (err) {
+        res.status(500).send("Error fetching users");
+    }
+})
+//deleting the user data
+app.delete("/user", async (req, res) => {
+    const userId = req.body.userId;
+    try {
+        await User.findByIdAndDelete({_id: userId});
+        // await User.findByIdAndDelete(userId); // this also works
+        res.send("User deleted successfully");
+    }
+    catch (error) {
+        console.error("Error deleting user:", error);
+        res.status(500).send("Error deleting user");
+    }
+})
+// updating the user data
+app.patch("/user", async (req, res) => {
+    const userId = req.body.userId;
+    const data = req.body;
+    try {
+        await User.findByIdAndUpdate(userId, data);
+        res.send("User updated successfully");
+    }
+    catch (error) {
+        console.error("Error updating user:", error);
+        res.status(500).send("Error updating user");
+    }
+});
+
 
 connectDB()
     .then(() => {
